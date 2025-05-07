fdddae0be858d64c6a56dc34f5a1b911bc4e1383
Ignacio <ignacio.pulice@fairgate.io>
2025-05-07 16:38:40 -0300

    Update storage + new configuration for create storage

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,11 @@
 use bitvmx_bitcoin_rpc::rpc_config::RpcConfig;
 use serde::Deserialize;
+use storage_backend::storage_config::StorageConfig;
 
 #[derive(Deserialize, Debug)]
 pub struct ConfigIndexer {
-    pub db_file_path: String,
-    pub rpc: RpcConfig,
+    pub storage: StorageConfig,
+    pub bitcoin: RpcConfig,
     pub checkpoint_height: Option<u32>,
     pub log_level: Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use anyhow::{Context, Result};
+use bitcoin::Network;
 use bitcoin_indexer::{
     config::ConfigIndexer,
     helper::define_height_to_sync,
     indexer::{Indexer, IndexerApi},
     store::{IndexerStore, StoreClient},
 };
+use bitcoind::bitcoind::Bitcoind;
 use bitvmx_bitcoin_rpc::{
     bitcoin_client::{BitcoinClient, BitcoinClientApi},
     types::BlockHeight,
 };
 use bitvmx_settings::settings;
-use tracing::info;
-use std::{path::PathBuf, rc::Rc, sync::mpsc::channel, thread, time::Duration};
+use std::{rc::Rc, sync::mpsc::channel, thread, time::Duration};
 use storage_backend::storage::Storage;
+use tracing::info;
 
 fn main() -> Result<()> {
     let (tx, rx) = channel();
@@ -23,24 +25,38 @@ fn main() -> Result<()> {
     let config = settings::load::<ConfigIndexer>()?;
 
     let log_level = match config.log_level {
-        Some(level) => {
-            level.parse().unwrap_or(tracing::Level::INFO)   
-        },
-        None => tracing::Level::INFO,     
+        Some(level) => level.parse().unwrap_or(tracing::Level::INFO),
+        None => tracing::Level::INFO,
     };
 
-    tracing_subscriber::fmt()
-        .with_max_level(log_level)
-        .init();
-  
-    let bitcoin_client = BitcoinClient::new_from_config(&config.rpc)?;
+    tracing_subscriber::fmt().with_max_level(log_level).init();
+
+    let bitcoind = Bitcoind::new(
+        "bitcoin-regtest",
+        "ruimarinho/bitcoin-core",
+        config.bitcoin.clone(),
+    );
+
+    info!("Starting bitcoind");
+    bitcoind.start()?;
+
+    let bitcoin_client = BitcoinClient::new_from_config(&config.bitcoin)?;
+
+    let wallet = bitcoin_client.init_wallet(Network::Regtest, "test_wallet");
+
+    if wallet.is_ok() {
+        let address = wallet.unwrap();
+        info!("Mining 100 blocks to wallet");
+        bitcoin_client.mine_blocks_to_address(100, &address)?;
+    }
+
     let blockchain_height = bitcoin_client.get_best_block()? as BlockHeight;
 
     let network = bitcoin_client.get_blockchain_info()?.chain;
     info!("Connected to chain {}", network);
     info!("Chain best block at {}H", blockchain_height);
 
-    let storage = Rc::new(Storage::new_with_path(&PathBuf::from(config.db_file_path))?);
+    let storage = Rc::new(Storage::new(&config.storage)?);
     let indexer_store = IndexerStore::new(storage)?;
     let best_block = indexer_store.get_best_block()?;
     let best_block_height = best_block.map(|block| block.height);
@@ -58,6 +74,7 @@ fn main() -> Result<()> {
     loop {
         if rx.try_recv().is_ok() {
             info!("Stop Bitcoin Indexer");
+            bitcoind.stop()?;
             break;
         }
 
@@ -70,6 +87,8 @@ fn main() -> Result<()> {
             prev_height = height_to_sync;
         }
     }
+
+    bitcoind.stop()?;
 
     Ok(())
 }
