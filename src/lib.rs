5ced2e7a5ac4515965f55869b51b7d169fca1a40
Ignacio <ignacio.pulice@fairgate.io>
2025-06-06 12:03:20 -0300

    Add indexer Type

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+use bitvmx_bitcoin_rpc::bitcoin_client::BitcoinClient;
+
+use crate::indexer::Indexer;
+
 pub mod config;
 pub mod errors;
 pub mod indexer;
 pub mod store;
+
+pub type IndexerType = Indexer<BitcoinClient>;
