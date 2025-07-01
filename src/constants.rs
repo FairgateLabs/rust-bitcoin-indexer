9eced64c801d8c1315e57af27fa9b24510d0e674
Ignacio <ignacio.pulice@fairgate.io>
2025-07-01 15:13:49 -0300

    Use type from rpc

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,4 @@
+use bitvmx_bitcoin_rpc::types::BlockHeight;
+
 // The default block height from which the indexer begins synchronization if no checkpoint is specified.
-pub const DEFAULT_CHECKPOINT_HEIGHT: u32 = 0;
+pub const DEFAULT_CHECKPOINT_HEIGHT: BlockHeight = 0;
